733bd94fd55a9ea0818c4ef0878319ee464b93a6
NathanStanding <standingtall1000@gmail.com>
2022-01-22 23:24:06 -0700

    beginning of a search bar

--- src/components/JumbotronComponent.js
+++ src/components/JumbotronComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Jumbotron } from 'reactstrap';
+import { Jumbotron, button } from 'reactstrap';
+
 
 class BuildJumbotron extends Component {
     render(){
@@ -10,6 +11,21 @@ class BuildJumbotron extends Component {
                         <div className="col">
                             <h1>Science</h1>
                             <h2>Activities for Your Classroom</h2>
+                        </div>
+                    </div>
+                </div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <div class="input-group">
+                                <div class="form-outline">
+                                    <input type="search" id="form1" class="form-control" />
+                                    <label class="form-label" for="form1">Search</label>
+                                </div>
+                                <button type="button" class="btn btn-primary">
+                                    <i class="fas fa-search"></i>
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
