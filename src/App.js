bc7c199c035a7052f4b99e50641caabae2118040
NathanStanding <standingtall1000@gmail.com>
2022-01-26 18:01:27 -0700

    Added Background Image

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component} from 'react';
 import './App.css';
+import background from "./images/background.jpg";
 
 import BuildNav from './components/NavbarComponent';
 import BuildJumbotron from './components/JumbotronComponent';
 import BuildFooter from './components/FooterComponent';
 
 class App extends Component {
+
     render() {
+
         return (
             <div className="App">
                 <BuildJumbotron />
                 <BuildNav/>
+                <div style={{ backgroundImage: `url(${background})` }}>
+                
+                </div>
                 <BuildFooter/>
             </div>
         );
